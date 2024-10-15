82527d63e5a5295cbd7d372bb344a4675485c739
GabrielMF771 <gabrielmartinsf771@gmail.com>
2024-10-15 15:41:36 -0300

    add InsertionSort.c

---
+++ Algoritmos de Ordenação/InsertionSort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <locale.h>
+
+#define TAMANHO 10
+
+// Função para implementar o Insertion Sort
+void insertionsort(int vet[], int tam) {
+    int i, j, chave;
+    
+    for (i = 1; i < tam; i++) {
+        chave = vet[i];  // Armazena o valor atual a ser inserido
+        j = i - 1;
+
+        // Move os elementos maiores que a chave uma posição à frente
+        while (j >= 0 && vet[j] > chave) {
+            vet[j + 1] = vet[j];
+            j--;
+        }
+        // Insere a chave na posição correta
+        vet[j + 1] = chave;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    int vet[TAMANHO] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, i;
+    
+    printf("Insertion Sort\n\n");
+    
+    printf("Vetor antes da ordenação: ");
+    for(i = 0; i < TAMANHO; i++){
+        printf("%d ", vet[i]);
+    }
+
+    printf("\n");
+
+    // Chama a função insertionsort para ordenar o vetor
+    insertionsort(vet, TAMANHO);
+
+    printf("\nVetor após a ordenação: ");
+    for(i = 0; i < TAMANHO; i++){
+        printf("%d ", vet[i]);
+    }
+
+    printf("\n");
+    
+    return 0;
+}
